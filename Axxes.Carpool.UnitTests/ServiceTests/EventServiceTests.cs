5436fc2c389578e37d8623d56f6bfdf77e23d208
Sander Philipsen <Sander.Philipsen@hotmail.com>
2023-08-18 11:04:00 +0200

    Completed

--- Axxes.Carpool.UnitTests/ServiceTests/EventServiceTests.cs
+++ Axxes.Carpool.UnitTests/ServiceTests/EventServiceTests.cs
@@ -12,11 +12,8 @@ public sealed class EventServiceTests
     private readonly Event _haxx = new("Haxx", "Internal conference", new DateTime(2023, 11, 4),
         new DateTime(2023, 11, 5), "Mallorca");
 
-    private readonly Event _ski = new("Ski", "Teambuilding weekend", new DateTime(2024, 04, 20),
-        new DateTime(2024, 04, 24), "Obertaurn");
-
-    private readonly Event _techorama = new("Techorama", ".NET conference", new DateTime(2024, 05, 10),
-        new DateTime(2024, 05, 23), "Kinepolis Antwerpen");
+    private readonly Event _haxxInPassed = new("Haxx", "Internal conference", new DateTime(2022, 11, 4),
+        new DateTime(2022, 11, 5), "Mallorca");
 
     [Fact]
     public void AddEvent_Should_Succeed_And_Not_Throw_An_Exception()
@@ -45,7 +42,27 @@ public sealed class EventServiceTests
     {
         // Arrange
         var eventCommand = new EventCommand("Haxx", "Internal conference", DateTime.Now.AddDays(5).AddHours(2),
-            DateTime.Now.AddDays(5).AddHours(5), "Grimbergen");
+            DateTime.Now.AddDays(2).AddHours(5), "Mallorca");
+
+        var eventRepository = new Mock<IEventRepository>();
+
+
+        var personRepository = new Mock<IPersonRepository>();
+        personRepository.Setup(p => p.GetAllPersons()).Returns(new List<Person>());
+
+        var eventService = new EventService(eventRepository.Object, personRepository.Object);
+
+        //Assert
+        Assert.Throws<StartDateTimeBeforeEndDateTimeException>(() => eventService.AddEvent(eventCommand));
+
+    }
+
+    [Fact]
+    public void Add_Event_Should_Throw_EventInPassedException_If_The_StartDateTime_Is_After_The_EndDateTime()
+    {
+        // Arrange
+        var eventCommand = new EventCommand("Haxx", "Internal conference", DateTime.Now.AddDays(5),
+            DateTime.Now.AddDays(7), "Mallorce");
 
 
         var eventRepository = new Mock<IEventRepository>();
@@ -60,7 +77,6 @@ public sealed class EventServiceTests
         Assert.Throws<EventAlreadyExistsException>(() => eventService.AddEvent(eventCommand));
 
     }
-
     [Fact]
     public void Register_For_Event_Should_Throw_Exception_If_License_Plate_Is_Invalid()
     {
@@ -119,4 +135,68 @@ public sealed class EventServiceTests
         //Assert
         Assert.Throws<EventNotFoundException>(() => eventService.RegisterForEvent(registerCommand));
     }
+
+    [Fact]
+    public void Register_For_Event_With_Person_Who_IsOpenToCarpool_And_Candrive_Should_Throw_LicensePlateNumberException_If_LicensePlate_Is_Not_Filled_In_Does_Not_Exists()
+    {
+        // Arrange
+
+        var eventRepository = new Mock<IEventRepository>();
+        eventRepository.Setup(p => p.GetAllEvents()).Returns(new List<Event>() { _haxx });
+
+        var personRepository = new Mock<IPersonRepository>();
+        personRepository.Setup(p => p.GetAllPersons()).Returns(new List<Person>() { new("Sander") });
+
+        var eventService = new EventService(eventRepository.Object, personRepository.Object);
+        var registerCommand = new RegisterCommand(_haxx.Id, "Sander", true, true, null);
+
+        //Assert
+
+        var ex = Record.Exception(() => eventService.RegisterForEvent(registerCommand));
+
+        Assert.IsType(typeof(LicensePlateNumberException), ex);
+        Assert.Equal(ex.Message, "License plate number is required when you want to carpool and can drive.");
+    }
+
+
+    [Fact]
+    public void Register_For_Event_That_Is_In_The_Passed_Should_Throw_PassedEventException()
+    {
+        // Arrange
+
+        var eventRepository = new Mock<IEventRepository>();
+        eventRepository.Setup(p => p.GetAllEvents()).Returns(new List<Event>() { _haxxInPassed });
+
+        var personRepository = new Mock<IPersonRepository>();
+        personRepository.Setup(p => p.GetAllPersons()).Returns(new List<Person>() { new("Sander") });
+
+        var eventService = new EventService(eventRepository.Object, personRepository.Object);
+        var registerCommand = new RegisterCommand(_haxxInPassed.Id, "Sander", true, true, "ABC-123");
+
+        //Assert
+        Assert.Throws<PassedEventException>(() => eventService.RegisterForEvent(registerCommand));
+    }
+
+    [Fact]
+    public void Register_For_Event_That_Is_Valid_Should_Not_Throw_Any_Exception()
+    {
+        // Arrange
+
+        var eventRepository = new Mock<IEventRepository>();
+        eventRepository.Setup(p => p.GetAllEvents()).Returns(new List<Event>() { _haxx });
+
+        var personRepository = new Mock<IPersonRepository>();
+        personRepository.Setup(p => p.GetAllPersons()).Returns(new List<Person>() { new("Sander") });
+
+        var eventService = new EventService(eventRepository.Object, personRepository.Object);
+
+        var registerCommand = new RegisterCommand(_haxx.Id, "Sander", true, true, "ABC-123");
+
+        //Act
+        var exception = Record.Exception(() => eventService.RegisterForEvent(registerCommand));
+
+        //Assert
+        Assert.Null(exception);
+    }
+
 }
